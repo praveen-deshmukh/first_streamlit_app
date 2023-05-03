544d97560683090e4ae6696393af847272c5e884
Praveen Deshmukh <113445408+praveen-deshmukh@users.noreply.github.com>
2023-05-03 20:07:58 +0530

    Update streamlit_app.py
    Fixed Typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,8 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Dinner.')
+streamlit.header('Breakfast Menu.')
+streamlit.text('Omega 3 & Blueberry Oatmeal.')
+streamlit.text('Kale, Spinach and Rocket Smoothie.')
+streamlit.text('Hard Boil Free-Range Egg.')
+
