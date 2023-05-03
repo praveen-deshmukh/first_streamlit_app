ddbd236afee818e31fadb3006c4ea8049ea443fa
Praveen Deshmukh <113445408+praveen-deshmukh@users.noreply.github.com>
2023-05-03 20:41:24 +0530

    Update streamlit_app.py
    Changes family to MOm's

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@
 import streamlit
 import pandas
 
-streamlit.title('My Parents New Healthy Dinner')
+streamlit.title("My Mom's New Healthy Dinner")
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach and Rocket Smoothie')
