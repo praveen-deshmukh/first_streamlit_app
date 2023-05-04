d3cb8b2eb5104ebb9b6e6796cf6cccb7d42c608c
Praveen Deshmukh <113445408+praveen-deshmukh@users.noreply.github.com>
2023-05-04 15:05:20 +0530

    Update streamlit_app.py
    added header and convert response into json

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,7 @@
 
 import streamlit
 import pandas
-import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+
 
 streamlit.title("My Mom's New Healthy Dinner")
 streamlit.header('Breakfast Menu')
@@ -23,5 +21,8 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show )
 
-#streamlit.header("Fruityvice Fruit Advice!")
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response).json()
+streamlit.header("Fruityvice Fruit Advice!")
 
