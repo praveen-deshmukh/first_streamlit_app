f39f4a908c9feab95ffc46d449f721af26e7b7e6
Praveen Deshmukh <113445408+praveen-deshmukh@users.noreply.github.com>
2023-05-05 18:52:51 +0530

    Update streamlit_app.py
    added colon to else

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,11 +28,11 @@ try:
      fruit_choice = streamlit.text_input('What fruit would you like information about?')
      if not fruit_choice:
         streamlit.error("Please select a fruit to get the information.")
-     else
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-        fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-        # Display normalized data into dataframes
-        streamlit.dataframe(fruityvice_normalized)
+     else:
+          fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+          fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+           # Display normalized data into dataframes
+          streamlit.dataframe(fruityvice_normalized)
 except URLError as e:
     streamlit.error()
    
